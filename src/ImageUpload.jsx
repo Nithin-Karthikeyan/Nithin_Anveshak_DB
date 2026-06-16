8e64d4977160f8e93a45a79be038d12e6ab61da5
Soham Warde <sohamwarde1@gmail.com>
2026-06-16 23:36:54 +0530

    change path

--- src/ImageUpload.jsx
+++ src/ImageUpload.jsx
@@ -45,7 +45,7 @@ const uploadImage = async () => {
     console.log("Uploaded:", data);
 
     // Step 2: Send to backend → Notion
-    const notionRes = await fetch("https://anveshak-db.vercel.app", {
+    const notionRes = await fetch("https://anveshak-db.vercel.app/create-bill", {
       method: "POST",
       headers: {
         "Content-Type": "application/json",
